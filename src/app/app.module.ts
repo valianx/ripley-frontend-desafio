0d61aaa03b62dc048a38fc3281834c9f260fe379
Valian <mgutiers3012@gmail.com>
2021-03-24 21:50:56 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { HomeComponent } from './components/home/home.component';
+import { WithdrawalsComponent } from './components/withdrawals/withdrawals.component';
+import { BalanceComponent } from './components/balance/balance.component';
+import { TransfersComponent } from './components/transfers/transfers.component';
+import { HistorialComponent } from './components/historial/historial.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    WithdrawalsComponent,
+    BalanceComponent,
+    TransfersComponent,
+    HistorialComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzButtonModule,
+    NzPageHeaderModule,
+    NzGridModule,
+    NoopAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
